188e2ad4015213698b1111fd37e55d354fff6520
JosueDurand96 <josue.durand8@gmail.com>
2019-11-14 00:11:58 -0500

    INTERFACE

--- app/src/main/java/com/example/kotlin_retrofit_rxjava_recyclerview_gson_json/ApiMovies.kt
+++ app/src/main/java/com/example/kotlin_retrofit_rxjava_recyclerview_gson_json/ApiMovies.kt
@@ -2,7 +2,9 @@ package com.example.kotlin_retrofit_rxjava_recyclerview_gson_json
 
 import android.database.Observable
 import com.example.kotlin_retrofit_rxjava_recyclerview_gson_json.data.MovieResponse
+import retrofit2.http.GET
 
 interface ApiMovies {
+    @GET("/filippella/Sample-API-Files/master/json/movies-api.json")
     fun getMovies(): Observable<MovieResponse>
 }
