93ff24cee1ab9a096d3f1ab9c2b4c890b8a4ce4a
JosueDurand96 <josue.durand8@gmail.com>
2019-11-14 00:06:11 -0500

    dependencias

---
+++ app/src/main/java/com/example/kotlin_retrofit_rxjava_recyclerview_gson_json/ApiMovies.kt
@@ -0,0 +1,5 @@
+package com.example.kotlin_retrofit_rxjava_recyclerview_gson_json
+
+interface ApiMovies {
+    fun getMovies()
+}
---
+++ app/src/main/java/com/example/kotlin_retrofit_rxjava_recyclerview_gson_json/data/MovieResponse.kt
@@ -0,0 +1,4 @@
+package com.example.kotlin_retrofit_rxjava_recyclerview_gson_json.data
+
+class MovieResponse {
+}
