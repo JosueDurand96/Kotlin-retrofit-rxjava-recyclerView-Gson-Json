fbcda63f1894e469ac808a4c20784d938a984694
JosueDurand96 <josue.durand8@gmail.com>
2019-11-14 00:15:21 -0500

    retrofit2

--- app/src/main/java/com/example/kotlin_retrofit_rxjava_recyclerview_gson_json/MainActivity.kt
+++ app/src/main/java/com/example/kotlin_retrofit_rxjava_recyclerview_gson_json/MainActivity.kt
@@ -2,11 +2,22 @@ package com.example.kotlin_retrofit_rxjava_recyclerview_gson_json
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val retrofit : Retrofit= Retrofit.Builder()
+            .baseUrl("https://raw.githubusercontent.com")
+            .addConverterFactory(GsonConverterFactory.create())
+            .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
+            .build()
+
+
     }
 }
